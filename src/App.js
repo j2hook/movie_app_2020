2a24e0f0193d5edb3700361081e68159d102d937
j2hook <j2hook@gmail.com>
2020-09-18 12:13:56 +0900

    Edit README.md

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
+function Food() {
+  return (
+    <h3>I love { fav }</h3>
+  )
+}
+
 function App() {
   return (
   <div>
     <h1>Hello!!!!!</h1>
-    </div>
+    <Food fav="김치" />
+    <Food fav="짜장면" />
+    <Food fav="짬뽕" />
+    <Food fav="만두" />
+    <Food fav="피자" />
+    <Food fav="치킨" />
+  </div>
   );
 }
 
