00a176a5953d99dba4db5f933d93725d22ef0b54
j2hook <j2hook@gmail.com>
2020-09-18 12:48:53 +0900

    readme.md edit

--- src/App.js
+++ src/App.js
@@ -6,16 +6,22 @@ function Food() {
   )
 }
 
+const foodLike = [
+  {
+    name : '김치',
+  },
+  {
+    name : '만두',
+  },
+  {
+    name : '피자',
+  }
+];
+
 function App() {
   return (
   <div>
     <h1>Hello!!!!!</h1>
-    <Food fav="김치" />
-    <Food fav="짜장면" />
-    <Food fav="짬뽕" />
-    <Food fav="만두" />
-    <Food fav="피자" />
-    <Food fav="치킨" />
   </div>
   );
 }
